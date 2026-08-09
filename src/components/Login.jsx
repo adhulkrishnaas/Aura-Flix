49e5b99958bbe904432739f365c13881adcbec97
Adhul Krishna A S <adhulsudhi@gmail.com>
2026-08-09 19:42:50 +0100

    style(auth): add signup form layout and styling, reuisng signin layout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
       <Header />
@@ -12,20 +17,35 @@ const Login = () => {
         />
       </div>
       <form className="absolute bg-black/90 w-3/12 my-36 mx-auto right-0 left-0 text-white  p-8 rounded-2xl ">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="m-2 p-4 my-4 w-full bg-gray-800 rounded-lg"
+          ></input>
+        )}
         <input
           type="text"
           placeholder="Email Address"
-          className="p-2 m-2  my-4 w-full bg-gray-800 rounded-lg"
+          className="p-4 m-2  my-4 w-full bg-gray-800 rounded-lg"
         ></input>
+
         <input
           type="password"
           placeholder="Password"
-          className="m-2 p-2 my-4 w-full bg-gray-800 rounded-lg"
+          className="m-2 p-4 my-4 w-full bg-gray-800 rounded-lg"
         ></input>
         <button className="p-4  mx-2 my-6 bg-red-600 w-full rounded-lg">
-          Sign In
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
+        <p className="p4-4 ml-2 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already Registered Sign In Now"}
+        </p>
       </form>
     </div>
   );
