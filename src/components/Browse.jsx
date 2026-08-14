3d237d423902218facdd6652fc0a94a8c9d0c1e2
Adhul Krishna A S <adhulsudhi@gmail.com>
2026-08-14 16:09:59 +0100

    feat(hooks): add useNowPlayingMovies custom hook

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,19 +1,13 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import { API_OPTIONS } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+import addNowPlayingMovies from "../utils/movieSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS,
-    );
-    const json = await data.json();
-    console.log(json);
-  };
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  useNowPlayingMovies();
   return (
     <div>
       <Header />
